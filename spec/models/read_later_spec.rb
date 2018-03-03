6f0cb93e30597232e8a2626f405d36a914692245
gouf <innocent.zero@gmail.com>
2018-03-03 23:13:27 +0900

    Add read_later model spec

--- spec/models/read_later_spec.rb
+++ spec/models/read_later_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ReadLater, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'saving record' do
+    it 'has decoded url' do
+      ReadLater.create(url: 'http%3A%2F%2Fexample.com')
+      expect(ReadLater.first.url).to eq 'http://example.com'
+    end
+
+    after(:each) { ReadLater.destroy_all }
+  end
 end
