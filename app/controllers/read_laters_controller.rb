e9ce0f4763fd11f297468a4f0a0306ad68daf7cb
gouf <innocent.zero@gmail.com>
2018-03-04 02:08:58 +0900

    Implement read_laters#push

--- app/controllers/read_laters_controller.rb
+++ app/controllers/read_laters_controller.rb
@@ -2,4 +2,14 @@ class ReadLatersController < ApplicationController
   def index
     @read_laters = ReadLater.all
   end
+
+  def push
+    @read_later = ReadLater.create!(read_laters_params)
+  end
+
+  private
+
+  def read_laters_params
+    params.require(:read_laters).permit(:url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'read_laters', to: 'read_laters#index'
 
+  post 'read_laters/push', to: 'read_laters#push'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
