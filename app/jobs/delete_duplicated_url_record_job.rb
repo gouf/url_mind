c38410b30f25244aad47c24f5b51aca147f4241c
gouf <innocent.zero@gmail.com>
2018-03-16 16:19:21 +0900

    Implement: delete ReadLater record that duplicated URL

---
+++ app/jobs/delete_duplicated_url_record_job.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Delete ReadLater record (receive order from DispatchDuplicatedUrlDeletionJob)
+class DeleteDuplicatedUrlRecordJob < ApplicationJob
+  queue_as :default
+
+  def perform(**args)
+    ReadLater.destroy(args[:id])
+  end
+end
