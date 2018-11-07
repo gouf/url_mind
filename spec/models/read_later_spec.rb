d077de80baba39f53115cb8c299f2152b7b83d0f
gouf <innocent.zero@gmail.com>
2018-11-07 21:54:15 +0900

    Add ReadLater's title spec

--- spec/models/read_later_spec.rb
+++ spec/models/read_later_spec.rb
@@ -3,10 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe ReadLater, type: :model do
+  let(:markdown_text) do
+    _ = <<-MARKDOWN.strip
+      [title A](http://example.com)
+      [title B](http://example.com)
+    MARKDOWN
+  end
+
   context 'saving record' do
+    let(:record) do
+      ReadLater.create!(
+        title: 'test record title',
+        url: 'http%3A%2F%2Fexample.com'
+      )
+    end
+
     it 'has decoded url' do
-      ReadLater.create(url: 'http%3A%2F%2Fexample.com')
-      expect(ReadLater.first.url).to eq 'http://example.com'
+      expect(record.url).to eq 'http://example.com'
+    end
+
+    it 'has title' do
+      ReadLater.bulk_push(markdown_text)
+      expect(ReadLater.first.title).to eq 'title A'
     end
 
     after(:each) { ReadLater.destroy_all }
@@ -16,5 +34,17 @@ RSpec.describe ReadLater, type: :model do
     subject { ReadLater.pop! }
 
     it { is_expected.to be_nil }
+  end
+
+  context 'and markdown text data' do
+    it 'can get urls' do
+      urls = ReadLater.extract_urls_from_markdown(markdown_text)
+      expect(urls).to match (['http://example.com'] * 2)
+    end
+
+    it 'can get title' do
+      titles = ReadLater.extract_titles_from_markdown(markdown_text)
+      expect(titles).to match %w[title\ A title\ B]
+    end
   end
 end
